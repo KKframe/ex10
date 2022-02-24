ca978d2706822877a8660524d9c4ce3c8b5f7067
KKframe <keawsung.fame@gmail.com>
2022-02-24 23:26:08 +0700

    Initial commit

---
+++ lib/Post.dart
@@ -0,0 +1,12 @@
+class Post{
+final String uname;
+final String imgU;
+final String imgPost;
+final String content;
+Post({
+required this.uname,
+required this.imgU,
+required this.imgPost,
+required this.content,
+});
+}
---
+++ lib/main.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'InstaFood',
+      theme: ThemeData(
+        primarySwatch: Colors.orange,
+      ),
+      home: HomePage(),
+    );
+  }
+}
+class HomePage extends StatefulWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(title: Text('InstaFood',style: TextStyle(fontSize: 30,fontFamily: 'Cooper Black'),)),
+        body: Container(color: Colors.teal,)
+    );
+  }
+}
+
+
